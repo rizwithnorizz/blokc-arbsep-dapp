6cf0ff5d806a900a96e9e3926790b587067a4b06
rizwithnorizz <rjan20155@gmail.com>
2024-02-04 21:53:42 +0800

    Added .env file

---
+++ hardhat.config.cjs
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    arbsep: {
+      url: process.env.VITE_JSON_RPC_URL,
+      accounts: [process.env.VITE_PRIVATE_KEY],
+    },
+  },
+};
